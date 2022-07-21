6d56a8ceb172b87e93acf17be694676ff016667a
MaganaAsiati <assia.magana@gmail.com>
2022-07-21 14:15:45 +0200

    fix linter

--- person.rb
+++ person.rb
@@ -1,14 +1,14 @@
 class Person
+  attr_reader :id
+
+  attr_accessor :name, :age
+
   def initialize(age, parent_permission: true, name: 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_reader :id
-
-  attr_accessor :name, :age
 
   def of_age?
     @age >= 18
