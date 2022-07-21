151d6345f0a826058cd1fa73901257fb5638f690
MaganaAsiati <assia.magana@gmail.com>
2022-07-21 13:14:36 +0200

    Constructor with name, age, and parent_permission as parameter. name and parent_permission are optional and have default values of Unknown and true.

--- person.rb
+++ person.rb
@@ -1,3 +1,9 @@
 class Person
   def initialize(age, parent_permission: true, name: 'Unknown')
-    
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  
