8da2cf1a6c24f5bbbe6fb9acac93ce919996f49c
MaganaAsiati <assia.magana@gmail.com>
2022-07-21 20:31:33 +0200

    create class CapitalizeDecorator inherit from Decorator and trim name

--- decorator.rb
+++ decorator.rb
@@ -14,4 +14,12 @@ class CapitalizeDecorator < Decorator
   end
 end
 
-
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length >= 10
+      @nameable.correct_name.slice(0, 10)
+    else
+      @nameable.correct_name
+    end
+  end
+end
