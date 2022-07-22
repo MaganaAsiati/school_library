8aea42526ac5d3237c1c595a40db4a7b4f1adff2
MaganaAsiati <assia.magana@gmail.com>
2022-07-22 14:21:02 +0200

    create the class Book with title and auter as instance vatiables

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require './nameable'
 require_relative './decorator'
 
 class Person < Nameable
-  attr_reader :id, 
+  attr_reader :id
 
   attr_accessor :name, :age
 
@@ -13,7 +13,6 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
-    
   end
 
   def of_age?
