a4323a5e1aae57b960be42003feafb206bf33b22
MaganaAsiati <assia.magana@gmail.com>
2022-07-23 11:08:38 +0200

    add option to Lets users choose an option.

--- main.rb
+++ main.rb
@@ -54,3 +54,26 @@ def create_rental(app)
   puts 'Rental created!'
 end
 
+def person_info(app)
+  puts 'What is the person\'s id?'
+  app.people.each_with_index do |person, index|
+    puts "#{index}. [#{person.class}] - Name: #{person.name} ID: #{person.id} Age: #{person.age}"
+  end
+  id = gets.chomp
+  app.all_rentals(id.to_i)
+end
+
+def list_options(app, input)
+  puts input
+  case input
+  when 1
+    puts 'List of books'
+    app.all_book
+  when 2
+    puts 'List of person'
+    app.all_person
+  else
+    puts 'Invalid input!'
+  end
+end
+
