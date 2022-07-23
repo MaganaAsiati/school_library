9e93ac94c3af6ea93aab5b8cfeebebe8c121e9b8
MaganaAsiati <assia.magana@gmail.com>
2022-07-23 11:11:59 +0200

    modify rental class  initialization

--- rental.rb
+++ rental.rb
@@ -1,8 +1,7 @@
 class Rental
-  attr_accessor :date
-  attr_reader :person, :book
+  attr_accessor :date, :person, :book
 
-  def initialize(date, person, book)
+  def initialize(date:, person:, book:)
     @date = date
     @person = person
     person.rentals << self
