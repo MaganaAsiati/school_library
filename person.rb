425e0c6b7f87cc74794eb42ecc7257d42a9d23a8
MaganaAsiati <assia.magana@gmail.com>
2022-07-23 11:11:14 +0200

    modify age initialization

--- person.rb
+++ person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   attr_accessor :name, :age
 
   # Jai mis name = '' au lieu de name: ''
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
