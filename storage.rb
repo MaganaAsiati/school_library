ae689e573c3dd859c417aebd2221a1ba66b4cc13
MaganaAsiati <assia.magana@gmail.com>
2022-07-28 10:39:42 +0200

    save book in books.json

--- storage.rb
+++ storage.rb
@@ -15,4 +15,18 @@ class Storage
     end
   end
 
+  def save_all_data(app)
+    save_books(app)
+    save_person(app)
+    save_rental(app)
+  end
+
+  def save_books(app)
+    return unless File.file?('books.json')
+
+    book_file = File.open('books.json', 'w')
+    book_file.write(JSON.generate(app.books))
+    book_file.close
+  end
+
   
