f6c7f1c966e8e792d0fb14783ddada475a16f85c
MaganaAsiati <assia.magana@gmail.com>
2022-07-28 10:52:28 +0200

    conferve datainput from person into  a json object

--- person.rb
+++ person.rb
@@ -7,13 +7,22 @@ class Person < Nameable
   attr_accessor :name, :age
 
   # Jai mis name = '' au lieu de name: ''
-  def initialize(age:, name: 'Unknown', parent_permission: true)
+  def initialize(age:,id: nil,  name: 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..1000)
+    @id = id.nil? ? Random.rand(1..1000) : id
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
+  end
+  def to_json(_args)
+    JSON.dump({
+                id: @id,
+                name: @name,
+                age: @age,
+                parent_permission: @parent_permission,
+                rentals: @rentals
+              })
   end
 
   def of_age?
