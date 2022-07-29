7f04bcaa898cce183393feae07621fef0163cf76
MaganaAsiati <assia.magana@gmail.com>
2022-07-29 11:05:13 +0200

    add the spec_helper.rb class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require_relative '../book'
+require_relative '../person'
+require_relative '../teacher'
+require_relative '../student'
+require_relative '../rental'
+require_relative '../classroom'
 
 require 'yaml'
