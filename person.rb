3f6c59c7ed284b7be86d9723693f6211a5a30563
MaganaAsiati <assia.magana@gmail.com>
2022-07-29 10:55:07 +0200

    make attribut being accessor

--- person.rb
+++ person.rb
@@ -4,10 +4,10 @@ require_relative './decorator'
 class Person < Nameable
   attr_reader :id, :rentals
 
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :parent_permission
 
   # Jai mis name = '' au lieu de name: ''
-  def initialize(age:,id: nil,  name: 'Unknown', parent_permission: true)
+  def initialize(age:, id: nil, name: 'Unknown', parent_permission: true)
     super()
     @id = id.nil? ? Random.rand(1..1000) : id
     @name = name
@@ -15,6 +15,7 @@ class Person < Nameable
     @parent_permission = parent_permission
     @rentals = []
   end
+
   def to_json(_args)
     JSON.dump({
                 id: @id,
@@ -29,7 +30,7 @@ class Person < Nameable
     @age >= 18
   end
 
-  def can_use_service?
+  def can_use_services?
     of_age? || @parent_permission
   end
 
